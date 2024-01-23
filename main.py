8b725d5551458214baf67129df23ca09a4c055fb
Vladimir (vapronva) <git@vprw.ru>
2024-01-23 22:52:33 +0300

    chore: update example to reflect changes to main client + minor fixes

--- main.py
+++ main.py
@@ -1,36 +1,50 @@
+import os
+
 from yandexgptapi.config import ModelURI
-from yandexgptapi.models import CompletionOptions, CompletionRequest, Message
+from yandexgptapi.models import (
+    CompletionOptions,
+    CompletionRequest,
+    CompletionResponse,
+    Message,
+    Operation,
+)
 from yandexgptapi.yandex_llm_client import YandexLLMClient
 
 if __name__ == "__main__":
-    iam_token = "your-iam-token"
-    folder_id = "your-folder-id"
+    iam_token: str = os.getenv("YANDEX_CLOUD_IAM_TOKEN", "")
+    folder_id: str = os.getenv("YANDEX_CLOUD_FOLDER_ID", "")
     request_payload = CompletionRequest(
         modelUri=f"gpt://{folder_id}/{ModelURI.YANDEX_GPT.value}",
         completionOptions=CompletionOptions(
             stream=False,
-            temperature=0.5,
-            maxTokens=2048,
+            temperature=0.6,
+            maxTokens=256,
         ),
         messages=[
-            Message(role="system", text=""),
-            Message(role="user", text=""),
+            Message(role="system", text="Ты - Саратов"),
+            Message(role="user", text="Кто?"),
         ],
     )
     # TextGeneration
     with YandexLLMClient(
-        iam_token=iam_token, folder_id=folder_id, timeout=10
+        iam_token=iam_token,
+        folder_id=folder_id,
+        timeout=10,
     ) as client:
-        client.headers = {"x-data-logging-enabled": "false"}
-        response = client.post_completion(request_data=request_payload)
+        response: CompletionResponse = client.post_completion(
+            request_data=request_payload
+        )
         print(response.alternatives[0].message.text)
     # TextGenerationAsync
     with YandexLLMClient(
-        iam_token=iam_token, folder_id=folder_id, timeout=10
+        iam_token=iam_token,
+        folder_id=folder_id,
+        timeout=10,
     ) as client:
-        client.headers = {"x-data-logging-enabled": "false"}
-        operation = client.post_completion_async(request_data=request_payload)
+        operation: Operation = client.post_completion_async(
+            request_data=request_payload
+        )
         if operation:
             print(f"Operation ID: {operation.id}")
-            response = client.wait_for_completion(operation.id)
+            response: CompletionResponse = client.wait_for_completion(operation.id)
             print(response.alternatives[0].message.text)
