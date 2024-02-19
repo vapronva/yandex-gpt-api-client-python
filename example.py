b9b22ced098e8519b77aa8886d531d92902e0a82
Vladimir (vapronva) <git@vprw.ru>
2024-02-19 15:44:26 +0300

    chore(example): update to reflect changes from 581abbc (apikey auth)

--- example.py
+++ example.py
@@ -1,4 +1,5 @@
-import os
+from os import getenv
+from time import sleep
 
 from yandexgptapi.config import GenerativeModelURI
 from yandexgptapi.models import (
@@ -14,10 +15,11 @@ from yandexgptapi.models import (
 from yandexgptapi.yandex_llm_client import YandexLLMClient
 
 if __name__ == "__main__":
-    iam_token: str = os.getenv("YANDEX_CLOUD_IAM_TOKEN", "")
-    folder_id: str = os.getenv("YANDEX_CLOUD_FOLDER_ID", "")
+    iam_token: str = getenv("YANDEX_CLOUD_IAM_TOKEN", "")
+    api_key: str = getenv("YANDEX_CLOUD_API_KEY", "")
+    folder_id: str = getenv("YANDEX_CLOUD_FOLDER_ID", "")
     request_payload = CompletionRequest(
-        modelUri=GenerativeModelURI.YANDEX_GPT.value.format(folder_id=folder_id),
+        modelUri=GenerativeModelURI.YANDEX_GPT_LITE.value.format(folder_id=folder_id),
         completionOptions=CompletionOptions(
             temperature=0.6,
             maxTokens=256,
@@ -27,7 +29,20 @@ if __name__ == "__main__":
             Message(role=MessageRole.SYSTEM, text="Кто?"),
         ],
     )
-    # TextGeneration
+    # auth with api key
+    with YandexLLMClient(
+        api_key=api_key,
+        folder_id=folder_id,
+        timeout=10,
+    ) as client:
+        # TextGeneration
+        print("{:=^50}".format("TextGeneration (API Key)"))
+        response_tg: CompletionResponse = client.post_completion(
+            request_data=request_payload,
+        )
+        print(response_tg)
+        sleep(2)
+    # auth with iam token
     with YandexLLMClient(
         iam_token=iam_token,
         folder_id=folder_id,
@@ -39,6 +54,7 @@ if __name__ == "__main__":
             request_data=request_payload,
         )
         print(response_tg)
+        sleep(2)
         # TextGeneration (stream)
         print("{:=^50}".format("TextGeneration (stream)"))
         response_tgs = client.post_completion_stream(
@@ -46,6 +62,7 @@ if __name__ == "__main__":
         )
         for chunk_response in response_tgs:
             print(chunk_response.alternatives[0].message.text)
+        sleep(2)
         # TextGenerationAsync
         print("{:=^50}".format("TextGenerationAsync"))
         operation_tga: Operation = client.post_completion_async(
@@ -54,17 +71,19 @@ if __name__ == "__main__":
         print(f"Operation ID: {operation_tga.id}")
         response_tga: CompletionResponse = client.wait_for_completion(operation_tga.id)
         print(response_tga)
+        sleep(2)
         # Tokenize
         print("{:=^50}".format("Tokenize"))
         response_t: TokenizeResponse = client.post_tokenize(
             request_data=TokenizeRequest(
                 modelUri=GenerativeModelURI.YANDEX_GPT.value.format(
-                    folder_id=folder_id
+                    folder_id=folder_id,
                 ),
                 text="Ты - Саратов",
             ),
         )
         print(response_t)
+        sleep(2)
         # TokenizeCompletion
         print("{:=^50}".format("TokenizeCompletion"))
         response_tc: TokenizeResponse = client.post_tokenize_completion(
